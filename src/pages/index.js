daa04d034dcc47ddc4e6efcc8cc3045d24122b93
saim <zasaimster@gmail.com>
2021-09-26 19:44:48 -0700

    added hero homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import HeroSection from "../components/HeroSection/Hero";
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/Sidebar/Sidebar";
 
@@ -13,6 +14,7 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
+      <HeroSection />
     </>
   );
 };
